b4b7f4f606ec1e35664e312eaddd02ab2c519cc0
ksuchitra532 <ksuchitra532@gmail.com>
2024-11-22 10:38:58 +0000

    fixing lts builds

--- build_robohelp_gh.bat
+++ build_robohelp_gh.bat
@@ -1,4 +1,5 @@
-@echo off
+@echo on
+setlocal enabledelayedexpansion
 rem experimental build
 if "%WORKSPACE%" == "" set WORKSPACE=%GITHUB_WORKSPACE%
 if "%BUILD_NUMBER%" == "" set BUILD_NUMBER=999
@@ -9,23 +10,51 @@ if "%JOB_NAME%" == "" set JOB_NAME=GMS2_Manual
 set BUILDTYPE=release
 set BUILDPLATFORM="Any CPU"
 set BUILDCLEAN=1
-set BUILDBETA=0
+set BUILDBETA=%BUILDBETA%
 set S3_BUCKET=%S3_BUCKET%
 set ZIP_FILE=%ZIP_FILE%
-echo on
 
 set basedir=%~dp0
 
 :check_options
-if "%1"=="release" ( 
-	set BUILDTYPE=release
-) else if "%1"=="debug" ( 
-	set BUILDTYPE=debug
-) else if "%1"=="noclean" ( 
-	set BUILDCLEAN=0
-) else if "%1"=="-beta" ( 
-	set BUILDBETA=1
-) else ( 
+if %LANGUAGE%==EN (
+    if "%BUILDBETA%"=="1" (
+        set robohelpPreset="GMS2 Manual Responsive HTML5 BETA"
+    ) else (
+        set robohelpPreset="GMS2 Manual Responsive HTML5"
+    )
+	goto finish_options
+) else if %LANGUAGE%==ES ( 
+	set robohelpPreset="GMS2 Manual Spanish"
+	goto finish_options
+) else if %LANGUAGE%==FR ( 
+	set robohelpPreset="GMS2 Manual French"
+	goto finish_options
+) else if %LANGUAGE%==DE ( 
+	set robohelpPreset="GMS2 Manual German"
+	goto finish_options
+) else if %LANGUAGE%==RU ( 
+	set robohelpPreset="GMS2 Manual Russian"
+	goto finish_options
+) else if %LANGUAGE%==PT-BR ( 
+	set robohelpPreset="GMS2 Manual Portuguese (Brazil)"
+	goto finish_options
+) else if %LANGUAGE%==ZH ( 
+	set robohelpPreset="GMS2 Manual Chinese (Simplified)"
+	goto finish_options
+) else if %LANGUAGE%==IT ( 
+	set robohelpPreset="GMS2 Manual Italian"
+	goto finish_options
+) else if %LANGUAGE%==JA ( 
+	set robohelpPreset="GMS2 Manual Japanese"
+	goto finish_options
+) else if %LANGUAGE%==PL ( 
+	set robohelpPreset="GMS2 Manual Polish"
+	goto finish_options
+) else if %LANGUAGE%==KO ( 
+	set robohelpPreset="GMS2 Manual Korean"
+	goto finish_options
+)else ( 
 	set DESTDIR=%1
 	goto finish_options
 )
@@ -48,10 +77,7 @@ copy "RoboHelpTool.exe" "%basedir%"
 set roboHelpTool="%basedir%Adobe_RoboHelp_2022\RoboHelp.exe"
 set helpTagsTool="%basedir%RoboHelpTool.exe"
 
-rmdir "%DESTDIR%\RoboHelp" /s /q
-
-set robohelpPreset="GMS2 Manual Responsive HTML5"
-if %BUILDBETA% == 1 set robohelpPreset="GMS2 Manual Responsive HTML5 BETA"
+echo "%robohelpPreset%"
 
 %roboHelpTool% --cl "%basedir%Manual\GMS2_Manual.rhpj" -o %robohelpPreset% -p "%DESTDIR%\RoboHelp" 
 
@@ -66,7 +92,21 @@ rem output help tags files
 rem append css fix to layout.css
 type "%basedir%SupportFiles\layout_fix_append.css" >> "%DESTDIR%\RoboHelp\template\Charcoal_Grey\layout.css"
 
-@REM rem ************************************************** ZIP up the output
 pushd %DESTDIR%\RoboHelp
+if /i %robohelpPreset%=="GMS2 Manual Responsive HTML5 BETA" (
+    echo Branch is develop - Choose BETA
+    aws s3 cp helpdocs_keywords.json s3://manual-json-files/Beta/helpdocs_keywords.json
+    aws s3 cp helpdocs_tags.json s3://manual-json-files/Beta/helpdocs_tags.json
+) else if /i %robohelpPreset%=="GMS2 Manual Responsive HTML5" (
+    echo Branch is Main - Choose Green
+    aws s3 cp helpdocs_keywords.json s3://manual-json-files/Green/helpdocs_keywords.json
+    aws s3 cp helpdocs_tags.json s3://manual-json-files/Green/helpdocs_tags.json
+) else (
+    echo Branch is not develop or main - Choose LTS
+    aws s3 cp helpdocs_keywords.json s3://manual-json-files/LTS/helpdocs_keywords.json
+    aws s3 cp helpdocs_tags.json s3://manual-json-files/LTS/helpdocs_tags.json
+)
+
+@REM rem ************************************************** ZIP up the output
 7z a YoYoStudioRoboHelp.zip . -r
 popd
